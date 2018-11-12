68a76e42e1f1fbe3bdb4e6d77cd15d6c20e1336c
azerbelhedi <azerbelhedide@gmail.com>
2018-11-12 19:27:40 +0100

    css build integration done

--- modules/commands/buildapp.js
+++ modules/commands/buildapp.js
@@ -4,7 +4,11 @@ var decomposers = require('../decomposers/decomposers.js') ;
 function buildApp(appName){
     console.log("building app ..." + appName);
     var htmlCode = file.fread("./"+appName+'/index.webx.html'); 
-    htmlCode = htmlCode.replace(/\s/g,''); 
+    var cssCode = file.fread("./"+appName+'/index.webx.css');
+    var rendredComponents = "";
+    console.log(htmlCode);
+    console.log(".................");
+
     var component = file.fread("./"+appName+"/stack.json");
     component = JSON.parse(component) ;
     var position ;
@@ -18,25 +22,52 @@ function buildApp(appName){
             console.log("infinit loop");
             break;
         }
-        for(var i = 1 ; i < component.length ; i++){
+        for(var i = 1 ; i < (component.length); i++){
+            if(rendredComponents.search(component[i].toUpperCase()) == -1){
+                rendredComponents += ("/" + component[i].toUpperCase() );
+                cssCode += '\n';
+                cssCode += file.fread("./"+appName+"/components/"+component[i].toLowerCase()+".css");
+            }
             component[i] = component[i].toUpperCase(); 
-            var componentCode = file.fread('./'+appName+'/components/'+component[i].toLocaleLowerCase()+'.html');
-            position = htmlCode.search("<"+component[i]+"/>") ;
+            var componentCode = file.fread('./'+appName+'/components/'+component[i].toLowerCase()+'.html');
+            position = htmlCode.search("<"+component[i].toUpperCase()) ;
+            if(position != -1){
+                var header = "";
+                var start = position+component[i].length+1;
+                while(htmlCode[start] != ">"){
+                    header+=htmlCode[start];
+                    start++;
+                }
+                console.log("identifier : "+header);
+            }
+            
             //console.log(position);
+            var mara = 0;
             while(position != -1){   
-                var thereIs = true ;
+                thereIs = true ;
                 //console.log(".."+component[i]);
-                htmlCode = htmlCode.replace("<"+component[i]+"/>",componentCode);
-                position = htmlCode.search("<"+component[i]+"/>") ;
+                htmlCode = htmlCode.replace("<"+component[i].toUpperCase()+header,
+                "<div " + header + ">" + 
+                    componentCode +
+                "</div");
+                position = -1 ;
+                position = htmlCode.search("<"+component[i].toUpperCase()+header) ;
+                console.log("mara : "+position) ; mara++;
+                console.log("<"+component[i].toUpperCase()+"/"+header);
+                //break;
             } 
         }
     }    
-    htmlCode = htmlCode.replace(/\s/g,''); 
+    //htmlCode = htmlCode.replace(/\s/g,''); 
 
 
     console.log(htmlCode);
     file.fdelete('./'+appName+'/index.html');
     file.fwrite('./'+appName+'/index.html',htmlCode);
+
+    file.fdelete('./'+appName+'/index.css');
+    file.fwrite('./'+appName+'/index.css',cssCode);
+
     console.log("your main page is : "+appName+"/index.html");
 }
 
